cfddfe5b8f4c3c84e56cadfebe7a84d84197ca58
Andrey <9400878@gmail.com>
2019-01-01 18:11:04 +0300

    Создание контекстного меню.

--- MyApplication/app/src/main/java/com/example/andrey/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/andrey/myapplication/MainActivity.java
@@ -3,14 +3,54 @@ package com.example.andrey.myapplication;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button one;
     private  TextView text;
+
+//    @Override
+//    public boolean onCreateOptionsMenu (Menu menu){
+//        getMenuInflater().inflate(R.menu.main_menu,menu);
+//        return true;
+//    }
+
+//    @Override
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+//        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case (R.id.app_bar_search):
+                Toast.makeText(this, "Search clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case (R.id.item_1):
+                Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case (R.id.item_2):
+                Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case (R.id.item_3):
+                Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +83,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (text.getText() == getString(R.string.cool)) {
                     text.setText(getString(R.string.hello_world));
+//                    Toast.makeText(MainActivity.this, "Это Привет!!!", Toast.LENGTH_SHORT).show();
                 }
-                else text.setText(getString(R.string.cool));
+                else {
+                    text.setText(getString(R.string.cool));
+                    Toast.makeText(MainActivity.this, "Это Круто!!!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
     }
 }
